f00da5717dea7b5518dcbfac56c6bcd58f7d7a1a
Bruno Santos <rapazcomumvl@hotmail.com>
2023-03-23 19:22:00 -0300

    criando contagem regressiva aniversário

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 AOS.init();
+
+const birthday = new Date("nov 23, 2023 06:00:00");
+const timeStempBirthday = birthday.getTime();
+
+const timeHours = setInterval(function () {
+    const currentTime = new Date();
+    const timeStempCurrent = currentTime.getTime();
+
+    const timeBirthday = timeStempBirthday - timeStempCurrent;
+
+    const dayMs = 1000 * 60 * 60 * 24;
+    const hoursMs = 1000 * 60 * 60;
+    const minMs = 1000 * 60;
+    const secMs = 1000;
+
+    const daysToEvent = Math.floor(timeBirthday / dayMs);
+    const hoursToEvent = Math.floor((timeBirthday % dayMs) / hoursMs);
+    const minToEvent = Math.floor((timeBirthday % hoursMs) / minMs);
+    const secToEvent = Math.floor((timeBirthday % minMs) / secMs);
+
+    document.getElementById('counter').innerHTML = `${daysToEvent}d ${hoursToEvent}h ${minToEvent}m ${secToEvent}s`;
+    if (timeBirthday < 0) {
+        clearInterval(timeHours);
+        document.getElementById("congratulations").innerHTML = `Chegou o grande dia ...Te espero hoje para comemorar com você por mais um ano de vida. Abraços e até lá!`;
+    }
+}, 1000);
